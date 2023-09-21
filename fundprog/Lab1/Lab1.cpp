3cd25e5c49119667d4bf075aa7124c8cd472cfe4
SmigaliovaAnastasia <angesie6151@gmail.com>
2023-09-21 14:32:00 +0300

    Added Lab1 fundprog

---
+++ fundprog/Lab1/Lab1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float x, y;
+	for (x = 0; x <= 40; x += 2)
+	{
+		if (x < 20)
+		{
+			y = 0.3 * x + 1 / x;
+		}
+		else {
+			y = sqrt(x * x + 3 * x) / 0.7;
+		}
+		cout << x << " " << y << endl;
+	}
+}
+	
+
