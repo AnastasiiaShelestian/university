f2b96d7b4be3960dabf41d42f282f99339b81e6c
AnastasiiaShelestian <shelestnest@gmail.com>
2023-10-18 19:46:18 +0300

    lab 3 so

--- SO/Lab3/script.bat
+++ SO/Lab3/script.bat
@@ -1,7 +1,27 @@
-©ECHO OFF
-set choice=2
-if %choice%==1 goto 
-if %choice%==2 goto
-:AGAIN Простой циклический командный файл
- ECHO Нажмите Ctrl-Break для остановки
-GOTO AGAIN
+@ECHO OFF
+
+choice /m "What do  you want to do with the file?" /c ctd /cs /d t /t 15 /n
+echo %ERRORLEVEL%
+if ERRORLEVEL 3 (
+  if NOT EXIST %1 (
+    echo File doesn't exist!
+    goto exitprogram
+  )
+  del %1
+)
+if ERRORLEVEL 2 (
+  if NOT EXIST %1 (
+    echo File doesn't exist!
+    goto exitprogram
+  )
+  type %1
+)
+if ERRORLEVEL 1 (
+  if EXIST %1 (
+    echo File already exists!
+    goto exitprogram
+  )
+  copy con %1
+) 
+
+:exitprogram
