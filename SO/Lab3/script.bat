3bffc14b652bf358e024ac0ba92b4828b9e2417c
AnastasiiaShelestian <shelestnest@gmail.com>
2023-10-19 02:13:36 +0300

    lab3_so

--- SO/Lab3/script.bat
+++ SO/Lab3/script.bat
@@ -1,27 +1,37 @@
-@ECHO OFF
+@echo off
+rem Скопировать имена всех текстовых файлов, которые начинаются на букву,
+rem заданную в качестве параметра, в файл rez.txt. Создать из этого списка
+rem "папки" и "файлы" в рабочем каталоге.
 
-choice /m "What do  you want to do with the file?" /c ctd /cs /d t /t 15 /n
-echo %ERRORLEVEL%
-if ERRORLEVEL 3 (
-  if NOT EXIST %1 (
-    echo File doesn't exist!
-    goto exitprogram
-  )
-  del %1
-)
-if ERRORLEVEL 2 (
-  if NOT EXIST %1 (
-    echo File doesn't exist!
-    goto exitprogram
-  )
-  type %1
-)
-if ERRORLEVEL 1 (
-  if EXIST %1 (
-    echo File already exists!
-    goto exitprogram
-  )
-  copy con %1
-) 
+rem "%~1" представляет %1 в виде строки. Если в %1 содержались кавычки, они убираются.
+rem Если %1 равен пустой строке, значит аргумент не был предоставлен.
+if "%~1"=="" (
+  rem Вывести сообщение об ошибке
+  echo "No letter provided. Please, provide a letter that files start with."
+  rem Перейти к концу программы
+  goto endprogram
+) else (
+  rem Иначе, выполняем программу из корневого каталога university (там содержится много файлов .txt для примеров)
+  cd c:/university/university/so
 
-:exitprogram
+  rem Удаляем файл rez.txt, если он уже существует, пересоздаем папку lab3\newfolder для копирования файлов
+  if exist rez.txt (
+    del rez.txt
+    rmdir /s /q lab3\newfolder
+    mkdir lab3\newfolder
+  )
+
+  rem Перебираем текстовые файлы, начинающиеся на букву из переменной letter, и копируем их имена в rez.txt
+  for /r %%F in (%1*.txt) do (
+    echo %%F >> rez.txt
+  )
+
+  rem Для всех файлов из файла rez.txt копируем их в lab3\newfolder\ используя их имя
+  for /f %%f in (rez.txt) do (
+    copy %%f lab3\newfolder\%%~nxf
+  )
+
+  echo Program complete.
+)
+cd c:/university/university/so/lab3
+:endprogram
