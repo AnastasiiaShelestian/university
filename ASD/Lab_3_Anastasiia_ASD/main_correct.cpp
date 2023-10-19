456efca3f01865052d00e53615faf8700dd36103
AnastasiiaShelestian <shelestnest@gmail.com>
2023-10-19 15:19:15 +0300

    Lab 3 asd

---
+++ ASD/Lab_3_Anastasiia_ASD/main_correct.cpp
@@ -0,0 +1,12 @@
+int hello(int somevalue) {
+  return somevalue;
+}
+
+int main() 
+{
+  hello(3);
+}
+
+/*
+  В данном случае функция объявлена, поэтому компиляция происходит успешно.
+*/
--- SO/Lab3/script2.bat
+++ SO/Lab3/script2.bat
@@ -1,4 +1,2 @@
 @echo off
-echo Start of execution script2.bat 
-call script.bat
-echo Batch file script.bat executed
+more textt.txt
